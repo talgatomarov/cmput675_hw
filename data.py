44c2d36de0b7516412db767be7a049279cf818c1
talgatomarov <talgat.omarov.1999@gmail.com>
2021-12-10 16:49:59 -0700

    Fix data size

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ class DataGenerator:
         self.p_max = p_max
 
     def generate(self, T, epsilon=1e-8):
-        w = np.random.uniform(0, epsilon)
-        v = np.random.uniform(self.p_min, self.p_max) * w
+        w = np.random.uniform(0, epsilon, size=T)
+        v = np.random.uniform(self.p_min, self.p_max, size=T) * w
 
         return v, w
