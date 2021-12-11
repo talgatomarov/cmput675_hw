9c9fcc71562078d71813d6334556d342d764b0ea
talgatomarov <talgat.omarov.1999@gmail.com>
2021-12-10 21:39:10 -0700

    Add multidimensional threshold algorithm.

--- model.py
+++ model.py
@@ -31,6 +31,44 @@ class ThresholdAlgorithm:
 
         return utility
 
+class MultidimensionalThresholdAlgorithm:
+    def __init__(self, p_min, p_max):
+        self.p_min = p_min
+        self.p_max = p_max
+
+        self.beta = 1 / (1 + np.log(p_max/p_min))
+
+    def phi(self, y):
+        if y < self.beta:
+            return self.p_min
+        elif self.beta <= y <= 1:
+            return self.p_min * np.exp(y/self.beta - 1)
+        else:
+            return 0
+
+    def solve(self, v, w):
+        assert len(v) == len(w)
+        T = len(v)
+        y_prev = 0
+        utility = 0
+
+        for t in range(T):
+
+            d = len(v[t])
+            value_density = v[0]/w[0]
+
+            for j in range(1, d):
+                value_density = max(value_density, v[j]/w[j])
+
+            if value_density >= self.phi(y_prev) and (y_prev + w[t]) <= 1.0:
+                utility += v[t]
+                y_prev += w[t]
+
+        return utility
+
+
+
+
 
 
 class OptimalAlgorithm:
